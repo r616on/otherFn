37fb70870d68903684c287c64a2ebba971e06bae
Prilepsky <r616on@gmail.com>
2022-04-21 08:13:09 +0300

    edit

--- src/Component/FuncToJsone/funcOnChangeHandler.js
+++ src/Component/FuncToJsone/funcOnChangeHandler.js
@@ -1,105 +1,128 @@
-function onChangeHandler({ values, fieldsMap }) {
-  const updatedValues = { ...values };
+function onChangeHandler({ errors, fieldsMap, values }) {
   const updatedFields = { ...fieldsMap };
-  const { existForceMajeure, exporterGuilty, providerGuilty, recStandart } =
-    updatedValues;
-  const editorReasonsList = [
-    existForceMajeure ? existForceMajeure[0] : null,
-    exporterGuilty ? exporterGuilty[0] : null,
-    providerGuilty ? providerGuilty[0] : null,
-    recStandart ? recStandart[0] : null,
-  ];
-  if (
-    [
-      "recStandart-yes",
-      "providerGuilty-yes",
-      "exporterGuilty-yes",
-      "existForceMajeure-yes",
-    ].some((value) => editorReasonsList.includes(value))
-  ) {
-    updatedFields.adjustmentDescription = {
-      ...updatedFields.adjustmentDescription,
-      constraints: [{ name: "NotNull" }],
-      validationSchema: {
-        required: { message: "Заполните поле", html: undefined, path: "" },
+  const updatedValues = { ...values };
+
+  if (updatedFields?.productTnvd) {
+    updatedFields.productTnvd.validationSchema = {
+      maxLength: {
+        value: 4,
+        message: "Не более 4 цифр в ТН ВЭД",
+        path: "",
       },
-      isNeedValidate: true,
-    };
-  } else {
-    updatedFields.adjustmentDescription = {
-      ...updatedFields.adjustmentDescription,
-      constraints: [],
-      validationSchema: {},
-      isNeedValidate: false,
     };
   }
-  if (
-    (updatedValues?.overall &&
-      +updatedValues.overall < 3 &&
-      +updatedValues.overall > 0) ||
-    (updatedValues?.deadlineConformity &&
-      +updatedValues.deadlineConformity < 3 &&
-      +updatedValues.overall > 0) ||
-    (updatedValues?.resultConformity &&
-      +updatedValues.resultConformity < 3 &&
-      +updatedValues.overall > 0) ||
-    (updatedValues?.recommendations &&
-      +updatedValues.recommendations < 3 &&
-      +updatedValues.overall > 0) ||
-    (updatedValues?.overallSatisfaction &&
-      +updatedValues.overallSatisfaction < 3 &&
-      +updatedValues.overall > 0)
-  ) {
-    if (updatedFields?.adjustment) {
-      updatedFields.adjustment.visible = true;
-    }
-    if (updatedFields?.adjustment2) {
-      updatedFields.adjustment2.visible = true;
-    }
-    if (updatedFields?.existForceMajeure) {
-      updatedFields.existForceMajeure.visible = true;
-    }
-    if (updatedFields?.exporterGuilty) {
-      updatedFields.exporterGuilty.visible = true;
-    }
-    if (updatedFields?.providerGuilty) {
-      updatedFields.providerGuilty.visible = true;
-    }
-    if (updatedFields?.recStandart) {
-      updatedFields.recStandart.visible = true;
-    }
-    if (updatedFields?.additionalServices) {
-      updatedFields.additionalServices.visible = true;
-    }
-    if (updatedFields?.adjustmentDescription) {
-      updatedFields.adjustmentDescription.visible = true;
-    }
-  } else {
-    if (updatedFields?.adjustment) {
-      updatedFields.adjustment.visible = false;
-    }
-    if (updatedFields?.adjustment2) {
-      updatedFields.adjustment2.visible = false;
-    }
-    if (updatedFields?.existForceMajeure) {
-      updatedFields.existForceMajeure.visible = false;
-    }
-    if (updatedFields?.exporterGuilty) {
-      updatedFields.exporterGuilty.visible = false;
-    }
-    if (updatedFields?.providerGuilty) {
-      updatedFields.providerGuilty.visible = false;
-    }
-    if (updatedFields?.recStandart) {
-      updatedFields.recStandart.visible = false;
-    }
-    if (updatedFields?.additionalServices) {
-      updatedFields.additionalServices.visible = false;
-    }
-    if (updatedFields?.adjustmentDescription) {
-      updatedFields.adjustmentDescription.visible = false;
-    }
+  if (updatedFields?.serviceOkved) {
+    updatedFields.serviceOkved.validationSchema = {
+      maxLength: {
+        value: 5,
+        message: "Не более 4 цифр в ОКВЭД",
+        path: "",
+      },
+    };
   }
 
-  return { updatedFields, updatedValues };
+  if (!!updatedValues.rowsNumber) {
+    if (updatedValues.serviceTableEditTitle !== "Изменить услугу") {
+      updatedValues.serviceTableEditTitle = "Изменить услугу";
+    }
+    if (updatedValues.productTableEditTitle !== "Изменить продукцию") {
+      updatedValues.productTableEditTitle = "Изменить продукцию";
+    }
+    const group = document.querySelector("[class^=KrGroupButtons]");
+    if (group) {
+      group.setAttribute("style", "display: none");
+      updatedValues.productSelectionButtons = null;
+    }
+  } else {
+    if (!updatedValues.productSelectionButtons) {
+      updatedValues.productSelectionButtons = "1";
+    }
+    if (updatedValues.serviceTableEditTitle === "Изменить услугу") {
+      updatedValues.serviceTableEditTitle = "Добавить услугу";
+    }
+    if (updatedValues.productTableEditTitle === "Изменить продукцию") {
+      updatedValues.productTableEditTitle = "Добавить продукцию";
+    }
+    const group = document.querySelector("[class^=KrGroupButtons]");
+    if (group) {
+      group.setAttribute("style", "display: flex");
+    }
+  }
+  if (updatedValues.productSelectionButtons === "1") {
+    const uploadedTnvd = updatedValues.productCatalog;
+    const uploadedOkved = updatedValues.serviceCatalog;
+    if (uploadedTnvd) {
+      updatedValues.productTnvd = "";
+      updatedValues.productTnvdName = null;
+      updatedValues.productCaption = uploadedTnvd.prodDescription;
+      updatedValues.productTnvd = uploadedTnvd.codeTnved;
+      updatedValues.productDescription =
+        uploadedOkved?.naming || updatedValues.productDescription;
+      updatedValues.productTnvdName = {
+        value: uploadedTnvd.codeTnved,
+        key: uploadedTnvd.key,
+      };
+    }
+    if (uploadedOkved) {
+      updatedValues.serviceOkved = "";
+      updatedValues.serviceOKVEDName = null;
+      updatedValues.serviceCaption = uploadedOkved.servDescription;
+      updatedValues.serviceOkved = uploadedOkved.codeOkved;
+      updatedValues.productDescription =
+        uploadedOkved?.naming || updatedValues.productDescription;
+      updatedValues.serviceOKVEDName = {
+        value: uploadedOkved.codeOkved,
+        key: uploadedOkved.key,
+      };
+    }
+  }
+  if (updatedValues.productSelectionButtons === "2") {
+    const newTnvd = updatedValues.productTnvdName;
+    const newOkved = updatedValues.serviceOKVEDName;
+    if (newTnvd) {
+      updatedValues.productCatalog = null;
+      updatedValues.productTnvd = newTnvd.codeTnved;
+    }
+    if (newOkved) {
+      updatedValues.serviceCatalog = null;
+      updatedValues.serviceOkved = newOkved.codeOcved;
+    }
+  }
+  if (!updatedValues.productCountry.value) {
+    updatedValues.productCountry = {
+      value: "Российская Федерация",
+      key: "21b35792-6d8e-4e04-a5d2-a73b9efa8b91",
+    };
+  }
+  if (updatedValues.contractAmount.length > 0) {
+    if (updatedFields?.amountJurContract) {
+      updatedFields.amountJurContract.validationSchema = {
+        required: {
+          html: undefined,
+          message: "Заполните поле",
+          path: undefined,
+        },
+      };
+    }
+  } else {
+    if (updatedFields?.amountJurContract) {
+      updatedFields.amountJurContract.validationSchema = {};
+    }
+  }
+  if (
+    updatedValues?.jurisdiction &&
+    updatedValues.jurisdiction.includes("jurisdictionHelp-yes")
+  ) {
+    updatedValues.jurisdiction = ["jurisdictionHelp-yes"];
+    if (
+      updatedValues?.arbitrationCourtInfo &&
+      updatedValues.arbitrationCourtInfo !== ""
+    ) {
+      updatedValues.arbitrationCourtInfo = "";
+    }
+    if (updatedValues?.stateCourtInfo) {
+      updatedValues.stateCourtInfo = {};
+    }
+  }
+  return { updatedValues, updatedFields, updatedErrors: { ...errors } };
 }
--- src/Component/FuncToJsone/test.js
+++ src/Component/FuncToJsone/test.js
@@ -1,39 +1,110 @@
-function onChangeHandler({ values, fieldsMap }) {
-  const updatedValues = { ...values };
+function onChangeHandler({ errors, fieldsMap, values }) {
   const updatedFields = { ...fieldsMap };
-  const { existForceMajeure, exporterGuilty, providerGuilty, recStandart } =
-    updatedValues;
-  const editorReasonsList = [
-    existForceMajeure ? existForceMajeure[0] : null,
-    exporterGuilty ? exporterGuilty[0] : null,
-    providerGuilty ? providerGuilty[0] : null,
-    recStandart ? recStandart[0] : null,
-  ];
-  if (
-    [
-      "recStandart-yes",
-      "providerGuilty-yes",
-      "exporterGuilty-yes",
-      "existForceMajeure-yes",
-    ].some((value) => editorReasonsList.includes(value))
-  ) {
-    updatedFields.adjustmentDescription = {
-      ...updatedFields.adjustmentDescription,
-      constraints: [{ name: "NotNull" }],
-      validationSchema: {
-        required: { message: "Заполните поле", html: undefined, path: "" },
+  const updatedValues = { ...values };
+
+  if (updatedFields?.productTnvd) {
+    updatedFields.productTnvd.validationSchema = {
+      maxLength: {
+        value: 4,
+        message: "Не более 4 цифр в ТН ВЭД",
+        path: "",
       },
-      isNeedValidate: true,
-    };
-  } else {
-    updatedFields.adjustmentDescription = {
-      ...updatedFields.adjustmentDescription,
-      constraints: [],
-      validationSchema: {},
-      isNeedValidate: false,
     };
   }
-  console.log("aaa");
+  if (updatedFields?.serviceOkved) {
+    updatedFields.serviceOkved.validationSchema = {
+      maxLength: {
+        value: 5,
+        message: "Не более 4 цифр в ОКВЭД",
+        path: "",
+      },
+    };
+  }
 
-  return { updatedFields, updatedValues };
+  if (!!updatedValues.rowsNumber) {
+    if (updatedValues.serviceTableEditTitle !== "Изменить услугу") {
+      updatedValues.serviceTableEditTitle = "Изменить услугу";
+    }
+    if (updatedValues.productTableEditTitle !== "Изменить продукцию") {
+      updatedValues.productTableEditTitle = "Изменить продукцию";
+    }
+    const group = document.querySelector("[class^=KrGroupButtons]");
+    if (group) {
+      group.setAttribute("style", "display: none");
+      updatedValues.productSelectionButtons = null;
+    }
+  } else {
+    if (!updatedValues.productSelectionButtons) {
+      updatedValues.productSelectionButtons = "1";
+    }
+    if (updatedValues.serviceTableEditTitle === "Изменить услугу") {
+      updatedValues.serviceTableEditTitle = "Добавить услугу";
+    }
+    if (updatedValues.productTableEditTitle === "Изменить продукцию") {
+      updatedValues.productTableEditTitle = "Добавить продукцию";
+    }
+    const group = document.querySelector("[class^=KrGroupButtons]");
+    if (group) {
+      group.setAttribute("style", "display: flex");
+    }
+  }
+  if (updatedValues.productSelectionButtons === "1") {
+    const uploadedTnvd = updatedValues.productCatalog;
+    const uploadedOkved = updatedValues.serviceCatalog;
+    if (uploadedTnvd) {
+      updatedValues.productTnvd = "";
+      updatedValues.productTnvdName = null;
+      updatedValues.productCaption = uploadedTnvd.prodDescription;
+      updatedValues.productTnvd = uploadedTnvd.codeTnved;
+      updatedValues.productDescription =
+        uploadedOkved?.naming || updatedValues.productDescription;
+      updatedValues.productTnvdName = {
+        value: uploadedTnvd.codeTnved,
+        key: uploadedTnvd.key,
+      };
+    }
+    if (uploadedOkved) {
+      updatedValues.serviceOkved = "";
+      updatedValues.serviceOKVEDName = null;
+      updatedValues.serviceCaption = uploadedOkved.servDescription;
+      updatedValues.serviceOkved = uploadedOkved.codeOkved;
+      updatedValues.productDescription =
+        uploadedOkved?.naming || updatedValues.productDescription;
+      updatedValues.serviceOKVEDName = {
+        value: uploadedOkved.codeOkved,
+        key: uploadedOkved.key,
+      };
+    }
+  }
+  if (updatedValues.productSelectionButtons === "2") {
+    const newTnvd = updatedValues.productTnvdName;
+    const newOkved = updatedValues.serviceOKVEDName;
+    if (newTnvd) {
+      updatedValues.productCatalog = null;
+      updatedValues.productTnvd = newTnvd.codeTnved;
+    }
+    if (newOkved) {
+      updatedValues.serviceCatalog = null;
+      updatedValues.serviceOkved = newOkved.codeOcved;
+    }
+  }
+  if (!updatedValues.productCountry.value) {
+    updatedValues.productCountry = {
+      value: "Российская Федерация",
+      key: "21b35792-6d8e-4e04-a5d2-a73b9efa8b91",
+    };
+  }
+  const { productTable = [] } = updatedValues;
+  if (productTable?.length >= 4) {
+    updatedFields.productTable.canAddRow = false;
+  } else {
+    updatedFields.productTable.canAddRow = true;
+  }
+  const { serviceTable = [] } = updatedValues;
+  if (serviceTable?.length >= 4) {
+    updatedFields.serviceTable.canAddRow = false;
+  } else {
+    updatedFields.serviceTable.canAddRow = true;
+  }
+  return { updatedValues, updatedFields, updatedErrors: { ...errors } };
 }
